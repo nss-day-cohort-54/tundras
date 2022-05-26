d7a83e9791642b3b8725ccb618dda67650810e22
hannahhall <hannahkhall13@gmail.com>
2022-05-26 13:50:11 -0500

    update and custom action

--- app_api/views/Movie.py
+++ app_api/views/Movie.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from app_api.models import Movie, Genre
 from django.db.models import Q
 from rest_framework import status
+from rest_framework.decorators import action
 class MovieView(ViewSet):
 
     def list(self, request):
@@ -29,13 +30,11 @@ class MovieView(ViewSet):
         """create"""
         user = request.auth.user
 
-        request.data['user'] = user.id
-
         movie = CreateMovieSerializer(data = request.data)
 
         movie.is_valid(raise_exception = True)
 
-        movie.save()
+        movie.save(user = request.auth.user)
 
 
         # genre = Genre.objects.get(pk=request.data['genre'])
@@ -53,17 +52,29 @@ class MovieView(ViewSet):
         return Response(movie.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk):
-        pass
+        
+        movie = Movie.objects.get(pk = pk)
+        serializer = CreateMovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user = request.auth.user)
 
-    def destroy(self, request, pk):
-        pass
+        return Response(None, status=status.HTTP_200_OK)
 
+
+    @action(methods=['get'], detail=False)
+    def my_movies(self, request):
+        """Return movies of the current user"""
+
+        user = request.auth.user
+        movies = Movie.objects.filter(user=user)
+        serialized = MovieSerializer(movies, many=True)
+        return Response(serialized.data)
 
 class CreateMovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model= Movie
-        fields= ('title', 'description', 'run_time', 'user', 'date_released', 'genre')
+        fields= ('title', 'description', 'run_time', 'date_released', 'genre')
 class MovieSerializer(serializers.ModelSerializer):
     """this will serialize data for movie views"""
     class Meta:
